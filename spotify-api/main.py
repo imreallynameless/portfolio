9d4c21378bf39ba3b77504135985a7c9682358fe
Lei Wu <lw2002@hotmail.ca>
2025-01-20 11:10:48 -0500

    Update main.py

--- spotify-api/main.py
+++ spotify-api/main.py
@@ -1,37 +1,42 @@
+from flask import Flask, jsonify
 import requests
+import os
 
-SPOTIFY_ACCESS_TOKEN = ''
+app = Flask(__name__)
+
+# Get the Spotify access token (from an environment variable or hard-code it for testing)
+SPOTIFY_ACCESS_TOKEN = 'BQA9ktweTOfQ7U6PXI-qMDCZ5MRHLoqp2h1P2NYQR858XbTUj1mcYKmisGXqHHGQvEfb-SzVRfJzWEDS9n4PgJpQnc1ONIQyU9fctUraTYRdmpjZss0'
 SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
 
-def get_current_track(access_token):
+def get_current_track():
     response = requests.get(
         SPOTIFY_GET_CURRENT_TRACK_URL,
         headers={
-            'Authorization': f'Bearer {access_token}'
+            'Authorization': f'Bearer {SPOTIFY_ACCESS_TOKEN}'
         }
     )
-    resp_json = response.json()
-    
-    track_id = resp_json['item']['id']
-    track_name = resp_json['item']['name']
-    artists = resp_json['item']['artists']
-    artists_name = ", ".join([artist['name'] for artist in artists])
-    track_link = resp_json['item']['external_urls']['spotify']
+    if response.status_code == 200:
+        resp_json = response.json()
+        track_id = resp_json['item']['id']
+        track_name = resp_json['item']['name']
+        artists = resp_json['item']['artists']
+        artists_name = ", ".join([artist['name'] for artist in artists])
+        track_link = resp_json['item']['external_urls']['spotify']
 
-    current_track_info = {
-        "id": track_id,
-        "name": track_name,
-        "artist": artists_name,
-        "link": track_link
-    }
+        return {
+            "id": track_id,
+            "name": track_name,
+            "artist": artists_name,
+            "link": track_link
+        }
+    else:
+        return {"error": "Unable to fetch the current track."}
 
-    return current_track_info
-
-def main():
-    current_track = get_current_track(SPOTIFY_ACCESS_TOKEN)
-    print(current_track)
+@app.route('/current-track', methods=['GET'])
+def current_track():
+    return jsonify(get_current_track())
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True, port=5000)
 
 
